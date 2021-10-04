15592989014b680d10b06bf75df15e8f6028a5ae
Tanvir0146 <tanvirhasan0146@gmail.com>
2021-10-04 11:19:57 +0600

    first commit

---
+++ LabWork/Models/Database.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using LabWork.Models.Tables;
+
+namespace LabWork.Models
+{
+    public class Database
+    {
+        public Students Students { get; set; }
+        public Courses Courses { get; set; }
+
+        public Database()
+        {
+            string connString = @"Server=RUSHDI\SQLEXPRESS;Database=Esite;Integrated Security=true";
+            SqlConnection conn = new SqlConnection(connString);
+            Students = new Students(conn);
+            Courses = new Courses();
+            
+        }
+        
+   
+    }
+}
